efd1fffd51e67910c25cf3cdca812e12094be08e
Andrew Morgan <amorgan@max-exfl015.desy.de>
2017-09-15 03:39:48 +0200

    added geom.py and Alexs geom

--- python/stream.py
+++ python/stream.py
@@ -2,27 +2,28 @@ import h5py
 import numpy as np
 import glob
 from enum import Enum
+import geom
 
 def slice_to_range(s):
     if s.start is None :
         start = 0
     else :
-        start = args.start
+        start = s.start
     if s.step is None :
         step = 1
     else :
-        step = args.step
+        step = s.step
     if s.stop is None :
         stop = len(self)
     else :
-        stop = args.stop
+        stop = s.stop
     return range(start, stop, step)
 
 class Stream():
     
     def __init__(self, calib, rundir):
         # dummy array for now
-        self.calib = calib
+        self.calib = np.transpose(calib, (3, 0, 2, 1))
         
         # grab the file names for each of the modules
         self.mod_fnams = self.get_mods(rundir)
@@ -30,11 +31,18 @@ class Stream():
         # initialise the iterator stuff
         self.index     = 0
         self.s_index   = 0
-        self.__len__   = 7500 * len(self.mod_fnams[0])
+        self.__len__   = 1500 // 8 * len(self.mod_fnams[0])
+    
 
         # create an empty array for holding the single frames
         self.shape  = (16, 512, 128)
         self.frame  = np.empty(self.shape, dtype=np.uint16)
+
+    def calibrate_frame(self, frame, index):
+        """
+        offset calib[mod, fs, ss, cell no.], cell no. = (i/2)%30
+        """
+        return frame - calib[index%7]
     
     # make self indexable: self[0] or self[40:100:2] etc
     def __getitem__(self, args):
@@ -50,11 +58,11 @@ class Stream():
         return self
     
     def __next__(self):
-        self.s_index = self.index // 7500 
+        self.s_index = self.index // 15000 
         if self.index == self.__len__ :
             raise StopIteration
-        self.index += 1
-        return self.get_frame(self.index-1)
+        self.index += 2*4
+        return self.get_frame(self.index-2*4)
         
     def get_mods(self, rundir):
         mods = []
@@ -63,13 +71,15 @@ class Stream():
             mod.sort()
             mods.append(mod)
         return mods
-
+    
     def get_frame(self, i):
-        ii = i % 7500
+        ii = i % 15000
+        print(ii)
         for m in range(16):
             DATAPATH = '/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%iCH0:xtdf/image/data' % m
-            self.frame[m] = h5py.File(self.mod_fnams[m][self.s_index], 'r')[DATAPATH][2*ii : 2*ii + 1, 0]
-        return self.frame
+            self.frame[m] = h5py.File(self.mod_fnams[m][self.s_index], 'r')[DATAPATH][ii, 0]
+        self.frame = self.calibrate_frame(self.frame, ii)
+        return self.frame.copy()
 
         
 if __name__ == '__main__':
@@ -77,7 +87,9 @@ if __name__ == '__main__':
     # this is {16, 128, 512, 7} {module, ss, fs, mem cell}
     calib  = h5py.File('../calibration/small_calibration.h5', 'r')['offsets'][()]
     rundir = '/gpfs/exfel/exp/SPB/201701/p002012/raw/r0006'
+    gfnam  = '/gpfs/exfel/u/scratch/SPB/201701/p002012/amorgan/EuXFEL/agipd_hmg2_oy0_man.geom'
     
-    stream = Stream(None, rundir)
-    frame  = stream.get_frame(0)
+    stream  = Stream(calib, rundir)
+    frames  = stream[:100:8]
+    gframes = np.array([geom.apply_geom(gfnam, frame) for frame in frames])
     
