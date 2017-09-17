20fa9bf7d403b9f3c4fae274f7d52118d66f5cfa
Filipe Maia <filipe.c.maia@gmail.com>
2017-09-17 07:00:02 +0200

    Add possiblity to do calibration and thresholding. Fix bug in train_id

--- python/combine_modules.py
+++ python/combine_modules.py
@@ -11,13 +11,15 @@ class AGIPD_Combiner():
     Initially specify path to folder with raw data
     Then use get_frame(num) to get specific frame
     '''
-    def __init__(self, folder_path, verbose=0, good_cells=[4,8,12,16,20,24,28]):
+    def __init__(self, folder_path, verbose=0, good_cells=[4,8,12,16,20,24,28],
+                 calib_file='/gpfs/exfel/exp/SPB/201701/p002012/scratch/filipe/offset_and_threshold.h5'):
         self.verbose = verbose
         self.good_cells = np.array(good_cells)*2
         self._make_flist(folder_path)
         self._get_nframes_list()
         self.frame = np.empty((16,512,128))
-
+        self.calib = h5py.File(calib_file,'r')
+        
     def _make_flist(self, folder_path):
         self.flist = np.array([np.sort(glob.glob('%s/RAW-*-AGIPD%.2d*.h5'%(folder_path, r))) for r in range(16)])
         try:
@@ -53,13 +55,34 @@ class AGIPD_Combiner():
         self.nframes = module_nframes[0]
         self.nframes_list = np.cumsum(self.nframes_list)
 
-    def _get_frame(self, num, type='frame'):
+    def _calibrate(self, data, gain, module, cell):        
+        data = np.float32(data)
+        high_gain = gain < self.calib['threshold'][module,0,cell,:,:]
+        low_gain = gain > self.calib['threshold'][module,1,cell,:,:]
+        medium_gain =  ~high_gain * ~low_gain
+        data -= self.calib['offset'][module,0,cell,:,:] * high_gain
+        data -= self.calib['offset'][module,1,cell,:,:] * medium_gain
+        data -= self.calib['offset'][module,2,cell,:,:] * low_gain
+        data[medium_gain] *= 45
+        data[low_gain] *= 45 * 3.8
+        data[data < -100] = 0
+        data[data > 10000] = 10000
+        return data
+
+
+    def _threshold(self, gain, module, cell):        
+        high_gain = gain < self.calib['threshold'][module,0,cell,:,:]
+        low_gain = gain > self.calib['threshold'][module,1,cell,:,:]
+        medium_gain =  ~high_gain * ~low_gain
+        return low_gain*2+medium_gain*1
+        
+    def _get_frame(self, num, type='frame', calibrate=False, threshold=False):
         if num > self.nframes or num < 0:
             print('Out of range')
             return
         
         cell_ind = num % len(self.good_cells)
-        train_ind = num // 60
+        train_ind = num // len(self.good_cells)
         
         if type == 'frame':
             ind = self.good_cells[cell_ind] + train_ind * 60
@@ -76,13 +99,21 @@ class AGIPD_Combiner():
         for i in range(16):
             with h5py.File(self.flist[i,file_num], 'r') as f:
                 dset_name = '/INSTRUMENT/SPB_DET_AGIPD1M-1/DET/%dCH0:xtdf/image/data'%i
-                self.frame[i] = f[dset_name][frame_num,0]
+                data = f[dset_name][frame_num,0]                
+                if calibrate:
+                    data = self._calibrate(data,
+                                           f[dset_name][frame_num+1,0],
+                                           i, cell_ind)
+                if threshold:
+                    data = self._threshold(data, i, cell_ind)
+                self.frame[i] = data
         return self.frame
 
-    def get_frame(self, num):
-        return self._get_frame(num,type='frame')
+    def get_frame(self, num, calibrate=False):
+        return self._get_frame(num,type='frame', calibrate=calibrate)
 
-    def get_gain(self, num):
-        return self._get_frame(num,type='gain')
+    def get_gain(self, num, threshold=False):
+        return self._get_frame(num,type='gain', calibrate=False, threshold=threshold)
 
 
+        
